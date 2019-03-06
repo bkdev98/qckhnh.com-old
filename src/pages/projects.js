af297f3dd22cf8d738dfd9c1367bc08eb30bf1bb
Quốc Khánh <bkdev98@gmail.com>
2019-03-06 12:10:43 +0700

    dreamy garden

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 
-import Section from '../components/section';
+import DreamyGarden from '../components/dreamy-garden';
 import Layout from '../components/layout';
-
-const Wrapper = styled(Section)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 14px;
-  letter-spacing: 4px;
-`;
 
 const Projects = () => (
   <Layout>
-    <Wrapper>
-    Code code...
-    </Wrapper>
+    <DreamyGarden />
   </Layout>
 );
 
