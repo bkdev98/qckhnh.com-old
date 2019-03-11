c3684614c903246073cbf30ceb22ef3e908509f9
Quốc Khánh <bkdev98@gmail.com>
2019-03-11 11:37:38 +0700

    update

--- src/components/blog-layout.js
+++ src/components/blog-layout.js
@@ -130,16 +130,8 @@ const Content = styled.div`
   z-index: 21;
   font-family: ${props => `${props.settings.font}, 'Roboto Mono', monospace`};
   font-weight: 300;
-  ::first-letter {
-    float: left;
-    color: #ffa7c4;
-    margin: 15px 2px 0 -5px;
-    line-height: .7;
-    font-size: 67px;
-    font-weight: 500;
-  }
-  p {
-    margin-bottom: 10px;
+  h1, h2, h3, h4, h5, p {
+    margin-bottom: 20px;
   }
   a {
     background-color: transparent;
